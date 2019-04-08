efbfb63809f58e3acd506a75c49046a041b48271
luis gomes <luisgustavogomes@outlook.com>
2019-04-08 17:12:18 -0300

    Criando Tabelas de dimensões e tabelas Fato

---
+++ DatawarehouseSucos/Tabelas/Dim_Fabrica.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Dim_Fabrica]
+(
+	[Cod_Fabrica] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [Desc_Fabrica] NVARCHAR(200) NULL
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Descrição das Fabricas',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Dim_Fabrica',
+    @level2type = N'COLUMN',
+    @level2name = N'Desc_Fabrica'
