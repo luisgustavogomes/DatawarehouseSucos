3de781981ee78073089f59a83a7de4a5595fb1ff
luis gomes <luisgustavogomes@outlook.com>
2019-04-09 07:42:29 -0300

    Criando tabelas de Fatos

--- DatawarehouseSucos/Tabelas/Fato_003.sql
+++ DatawarehouseSucos/Tabelas/Fato_003.sql
@@ -1,4 +1,9 @@
 ﻿CREATE TABLE [dbo].[Fato_003]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+    [Cod_Fabrica] NVARCHAR(50) NOT NULL, 
+    [Cod_Dia] NVARCHAR(50) NOT NULL, 
+    [Custo] DECIMAL(18, 4) NULL, 
+    PRIMARY KEY ([Cod_Fabrica], [Cod_Dia]), 
+	CONSTRAINT [FK_Fato_003_Dim_Fabrica] FOREIGN KEY (Cod_Fabrica) REFERENCES Dim_Fabrica(Cod_Fabrica), 
+	CONSTRAINT [FK_Fato_003_Dim_Tempo] FOREIGN KEY (Cod_Dia) REFERENCES Dim_Tempo(Cod_dia) 
 )
